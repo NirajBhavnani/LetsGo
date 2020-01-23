4725ad17234319660ec1106e7ec951a419aa0c2c
NirajBhavnani <neerajbhavnani24@gmail.com>
2020-01-23 18:18:25 +0530

    Pick and drop

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -7,6 +7,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -19,18 +20,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: Colors.red[50],
       appBar: AppBar(
-      backgroundColor: Colors.red[400],
-      title: Text("Let's Go"),
-      elevation: 0.0,
-      actions: <Widget>[
-        FlatButton.icon(
-          icon: Icon(Icons.person),
-          label: Text('Logout'),
-          onPressed: () async {
-            await _auth.signOut();
-          },
-        )
-      ],
+        backgroundColor: Colors.red[400],
+        title: Text("Let's Go"),
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Logout'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
       ),
       body: Map(),
       drawer: Drawer(
@@ -81,44 +82,137 @@ class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: <Widget>[
-          GoogleMap(
-            initialCameraPosition: CameraPosition(target: _initPosition , zoom: 10.0),
-            onMapCreated: onCreated,
-            myLocationEnabled: true,
-            mapType: MapType.normal,
-            compassEnabled: true,
-            markers: _markers,
-            onCameraMove: _onCameraMove,
-                                  ),
-                                  Positioned(top: 40, right: 10, 
-                                  child: FloatingActionButton(
-                                    onPressed: _onAddMarkerPressed,
-                                    tooltip: "Add marker",
-                                    backgroundColor: Colors.black,
-                                    child: Icon(Icons.add_location, color: Colors.white,),
-                                 ),)
-                              ],
-                                   );
-                                 }
-                                                            
-                                                              void onCreated(GoogleMapController controller) {
-                                                                setState(() {
-                                                                  mapController = controller;
-                                                                });
-                                                  }
-                                                
-                                                  void _onCameraMove(CameraPosition position) {
-                                                    setState(() {
-                                                      _lastPosition = position.target;
-                                                    });
-                                      }
-                                    
-                                      void _onAddMarkerPressed() {
-                                        setState(() {
-                                          _markers.add(Marker(markerId: MarkerId(_lastPosition.toString()), position: _lastPosition, 
-                                          infoWindow: InfoWindow(title: "Remember Here", snippet: "Good Place"),
-                                          icon: BitmapDescriptor.defaultMarker ));
-                                        });
+      children: <Widget>[
+        GoogleMap(
+          initialCameraPosition:
+              CameraPosition(target: _initPosition, zoom: 10.0),
+          onMapCreated: onCreated,
+          myLocationEnabled: true,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          markers: _markers,
+          onCameraMove: _onCameraMove,
+        ),
+
+        Positioned(
+                  top: 50.0,
+                  right: 15.0,
+                  left: 15.0,
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(3.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(1.0, 5.0),
+                            blurRadius: 10,
+                            spreadRadius: 3)
+                      ],
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      //controller: appState.locationController,
+                      decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20, top: 5),
+                          width: 10,
+                          height: 10,
+                          child: Icon(
+                            Icons.location_on,
+                            color: Colors.black,
+                          ),
+                        ),
+                        hintText: "pick up",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  top: 105.0,
+                  right: 15.0,
+                  left: 15.0,
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(3.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(1.0, 5.0),
+                            blurRadius: 10,
+                            spreadRadius: 3)
+                      ],
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      //controller: appState.destinationController,
+                      textInputAction: TextInputAction.go,
+                      onSubmitted: (value) {
+                        //appState.sendRequest(value);
+                      },
+                      decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20, top: 5),
+                          width: 10,
+                          height: 10,
+                          child: Icon(
+                            Icons.local_taxi,
+                            color: Colors.black,
+                          ),
+                        ),
+                        hintText: "destination?",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+
+        // Positioned(
+        //   top: 40,
+        //   right: 10,
+        //   child: FloatingActionButton(
+        //     onPressed: _onAddMarkerPressed,
+        //     tooltip: "Add marker",
+        //     backgroundColor: Colors.black,
+        //     child: Icon(
+        //       Icons.add_location,
+        //       color: Colors.white,
+        //     ),
+        //   ),
+        // )
+      ],
+    );
+  }
+
+  void onCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastPosition.toString()),
+          position: _lastPosition,
+          infoWindow: InfoWindow(title: "Remember Here", snippet: "Good Place"),
+          icon: BitmapDescriptor.defaultMarker));
+    });
   }
 }
